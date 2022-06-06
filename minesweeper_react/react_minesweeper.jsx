95366adeb6b6afa8e538dbae4a212311053290ca
miker704 <miker704@gmail.com>
2022-06-06 09:59:16 -0400

    updated react_minesweeper.jsx

--- minesweeper_react/react_minesweeper.jsx
+++ minesweeper_react/react_minesweeper.jsx
@@ -2,3 +2,10 @@ import Game from "./components/game";
 import React from "react";
 import { ReactDOM } from "react";
 
+ReactDOM.render(
+
+    <p>HelloWorld</p>,
+    document.getElementById('root')
+
+
+);
